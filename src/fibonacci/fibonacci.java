2858a7e26b27008707860d1129d090da2ec17978
nurhayatketenci <nurhayatktnn8@gmail.com>
2022-11-19 22:06:57 +0300

    commit

---
+++ src/fibonacci/fibonacci.java
@@ -0,0 +1,27 @@
+package fibonacci;
+
+import java.util.Scanner;
+
+public class fibonacci {
+    static int fib(int n)
+    {
+        if (n <= 1)
+            return n;
+  
+        // Recursive call
+        return fib(n - 1)
+            + fib(n - 2);
+    }
+  
+    public static void main(String args[])
+    {
+    	Scanner input=new Scanner(System.in);
+		System.out.println("Enter the number");
+        int number = input.nextInt();
+  
+        for (int i = 0; i < number; i++) {
+  
+            System.out.print(fib(i) + " ");
+        }
+    }
+}
